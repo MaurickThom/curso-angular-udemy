25b1ea94d55a4a69f5ef86c12ee5eab0fe84362a
MaurickThom <thomtwd@gmail.com>
2019-10-29 23:00:13 -0500

    Api server

--- crud1/server.ts
+++ crud1/server.ts
@@ -6,12 +6,33 @@ const express = require('express'),
 const readFileAsync = promisify(readFile),
         writeFileAsync = promisify(writeFile) 
 
-let db = {}
+let db = []
 
-app.get('/api',(req,res)=>{
-    res.json({
-        hola:'hola'
+const loadingDB = async()=>{
+    try{
+        db = JSON.parse(await readFileAsync(`database.json`))
+    }catch{
+        db = []
+    }
+    return db
+}
+
+const saveDB = ()=>{
+    return new Promise((resolve,reject)=>{
+        let data =JSON.stringify(db)
+        writeFile('database.json',data,err=>{
+            if(err) return reject(err)
+            return resolve(data)
+        })
     })
+}
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.get('/api/users',async(req,res)=>{
+    await loadingDB()
+    res.json(db)
 })
 
 app.listen(3000,()=>console.log('listen in port 3000'))
