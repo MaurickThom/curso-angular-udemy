283b0f332962e3359b3eddbbb13bb9e0986c3186
MaurickThom <thomtwd@gmail.com>
2019-11-06 17:54:01 -0500

    directives

--- directives/src/app/directives/my-directive.directive.ts
+++ directives/src/app/directives/my-directive.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2, Input, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appMyDirective]'
 })
 export class MyDirectiveDirective {
 
-  constructor() { }
+  // @Input() defaultColor;
+  // @HostBinding('style.color') color:string = this.defaultColor
+  constructor(
+    private elementRef:ElementRef,
+    private renderer:Renderer2
+  ) { 
+    // elementRef.nativeElement.style.backgroundColor = 'yellow'
+  }
+  ngOnInit(): void {
+    // this.color = this.defaultColor
+  }
 
+  @HostListener('mouseenter') mouseover(){
+    this.renderer.setStyle(this.elementRef.nativeElement,'background-color','red')
+  }
+
+  @HostListener('mouseleave') mouseleave(){
+    this.renderer.setStyle(this.elementRef.nativeElement,'background-color', 'transparent');
+  }
 }
