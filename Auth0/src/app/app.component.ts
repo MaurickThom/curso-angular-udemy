ee30eaaa388078de616f553465887bddef891dd6
MaurickThom <thomtwd@gmail.com>
2019-11-26 15:27:09 -0500

    injections

--- Auth0/src/app/app.component.ts
+++ Auth0/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Auth0';
+  constructor(
+    private auth:AuthService // en este caso este servicio inyectado no puede ser accedido en el html de este componente
+  ){
+    // muy diferente seria que este publico, en ese caso si estaria disponible el la variable de este servicio en el html del componente
+  }
 }
