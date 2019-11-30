322940e51c7dfe93325c5406e6e430dcaac5a562
MaurickThom <thomtwd@gmail.com>
2019-11-30 18:12:39 -0500

    Guards

--- LoginWithFirebase/src/app/app-routing.module.ts
+++ LoginWithFirebase/src/app/app-routing.module.ts
@@ -4,13 +4,25 @@ import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './guards/auth.guard';
+import { VerifyGuard } from './guards/verify.guard';
 
 
 const routes: Routes = [
-  { path: 'home'    , component: HomeComponent ,
-canActivate:[AuthGuard]},
-  { path: 'register', component: RegisterComponent },
-  { path: 'login'   , component: LoginComponent },
+  {
+    path: 'home',
+    component: HomeComponent ,
+    canActivate:[AuthGuard]
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate:[VerifyGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate:[VerifyGuard]
+  },
   {
     path:"",
     pathMatch:"full",
