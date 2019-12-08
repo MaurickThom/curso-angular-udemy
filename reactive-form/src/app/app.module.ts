cf39ea02beac7f78d1497a29f4c7fba82338ed05
MaurickThom <thomtwd@gmail.com>
2019-12-08 01:30:30 -0500

    modules

--- reactive-form/src/app/app.module.ts
+++ reactive-form/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TemplateComponent } from './components/template/template.component';
+import { DataComponent } from './components/data/data.component';
+import {HttpClientModule} from '@angular/common/http'
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
