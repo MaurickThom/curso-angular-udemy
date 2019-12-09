aa8cda15bd874ea72d68a29a50f83634e222b9ba
MaurickThom <thomtwd@gmail.com>
2019-12-08 21:21:14 -0500

    form x template

--- reactive-form/src/app/components/data/data.component.ts
+++ reactive-form/src/app/components/data/data.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
+  title="Data"
   constructor() { }
 
   ngOnInit() {
--- reactive-form/src/app/components/template/template.component.ts
+++ reactive-form/src/app/components/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+  title="Template"
   constructor() { }
 
   ngOnInit() {
   }
-
+  submit(form:NgForm){
+    console.log(form);
+  }
 }
