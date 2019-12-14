f5b6592ffdb032c4bbd0afed257d286024756b93
MaurickThom <thomtwd@gmail.com>
2019-12-14 01:06:16 -0500

    service api The Movies DB

--- MoviesApp/src/app/app.component.ts
+++ MoviesApp/src/app/app.component.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { ApiMovieDBService } from './services/api-movie-db.service';
+import { fromEvent } from 'rxjs';
+import { debounceTime, filter, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements AfterViewInit {
+  @ViewChild('input',{
+    static:false
+  }) input: ElementRef;
   title = 'MoviesApp';
   constructor(
     public api:ApiMovieDBService
   ){
-    this.api.getTheMostPopularMovies().subscribe(data=>console.log(data))
+    this.api.popularMoviesForKids().subscribe(data=>console.log(data))
+
+  }
+  ngAfterViewInit() {
+    fromEvent(this.input.nativeElement,'keyup')
+      .pipe(
+        filter(Boolean),
+        debounceTime(1000),
+        distinctUntilChanged()
+      )
+      .subscribe((data:any)=>{
+        const value = data.target.value
+        if(value.trim())
+          this.api.searchMovie(value.trim()).subscribe((data:any)=>{
+            const id = data.results[0].id
+            console.log(data);
+            this.api.getMovieById(id).subscribe(movie=>console.log(movie))
+          })
+      })
+
   }
 }
--- MoviesApp/src/app/services/api-movie-db.service.ts
+++ MoviesApp/src/app/services/api-movie-db.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
-import {map} from 'rxjs/operators'
+import {map, debounceTime, distinctUntilChanged, filter} from 'rxjs/operators'
 import { HttpClient,JsonpClientBackend,JsonpInterceptor } from '@angular/common/http';
 
+// https://developers.themoviedb.org/3 --> DOCUMENTATION API
 @Injectable({
   providedIn: 'root'
 })
 export class ApiMovieDBService {
   private API_KEY:string = '9df68693d8312376ef0de9f8f352a9fb'
-  private URL_MOVIE_DB:string = 'https://api.themoviedb.org/3'
+  private URL_MOVIE_DB:string = 'http://api.themoviedb.org/3'
   constructor(
     private api:HttpClient,
     private jsonp:JsonpClientBackend
@@ -17,6 +18,26 @@ export class ApiMovieDBService {
     const URL = `${this.URL_MOVIE_DB}/discover/movie?sort_by=popularity.desc&api_key=${this.API_KEY}&language=es`
     // const URL = `${this.URL_MOVIE_DB}/discover/movie?sort_by=popularity.desc&api_key=${this.API_KEY}&language=es&callback=JSONP_CALLBACK`
     return this.api.jsonp(URL,'callback')
-
+  }
+  searchMovie(title:string,page:number=1){
+    const URL = `${this.URL_MOVIE_DB}/search/movie?api_key=${this.API_KEY}&language=es&query=${title}&page=${page}&sort_by=popularity.desc`
+    return this.api.jsonp(URL,'callback').pipe(
+      filter(Boolean),
+      debounceTime(1000)
+    )
+  }
+  getMovieById(movieID:string){
+    const URL = `${this.URL_MOVIE_DB}/movie/${movieID}?api_key=${this.API_KEY}&language=es`
+    return this.api.jsonp(URL,'callback')
+  }
+  cinemaBillboard(){
+    const URL = `${this.URL_MOVIE_DB}/movie/now_playing?api_key=${this.API_KEY}&language=es&region=ES`
+    return this.api.jsonp(URL,'callback')
+  }
+  popularMoviesForKids(){
+    const URL = `${this.URL_MOVIE_DB}/discover/movie?sort_by=popularity.desc&api_key=${this.API_KEY}` +
+            `&language=es&region=ES&certification_country=ES` +
+            `&certification=APTA`;
+    return this.api.jsonp(URL,'callback')
   }
 }
