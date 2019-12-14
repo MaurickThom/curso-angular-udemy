f38e3dd54867869e12d5d1f1386f228290be5445
MaurickThom <thomtwd@gmail.com>
2019-12-14 01:53:12 -0500

    components

--- MoviesApp/src/app/app.module.ts
+++ MoviesApp/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule,HttpClientJsonpModule} from '@angular/common/http'
-import {FormsModule,ReactiveFormsModule} from '@angular/forms'
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { MovieCardComponent } from './components/cards/movie-card/movie-card.component';
+import { MoviePresentationComponent } from './components/presentation/movie-presentation/movie-presentation.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    MovieCardComponent,
+    MoviePresentationComponent
   ],
   imports: [
     BrowserModule,
