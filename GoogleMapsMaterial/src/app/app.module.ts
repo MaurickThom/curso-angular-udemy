ab336a29b1d12a0d67e3863c5ddeadeb11a507cb
MaurickThom <thomtwd@gmail.com>
2019-12-15 15:47:56 -0500

    comentario chido

--- GoogleMapsMaterial/src/app/app.module.ts
+++ GoogleMapsMaterial/src/app/app.module.ts
@@ -25,7 +25,8 @@ import {AgmCoreModule} from '@agm/core'
     ReactiveFormsModule,
     AngularMaterialModule,
     AgmCoreModule.forRoot({ // hola , si quieres puedes usar esta API_KEY pero por las huevas es , ya que se necesita habilitar una cuenta de
-                            // facturación y ta bien webon , estos poryectos son solo pruebas xd .
+                            // facturación y ta bien webon , estos proyectos son solo pruebas xd . El tráfico que genero al usar las APIs de Google
+                            // son casi nulas casi nada . Asi que me la pelas Google 7u7
       apiKey:'AIzaSyB0WrBOLAZ2PDdoqkGveIOVQU0QvXDHm7U'
     })
   ],
