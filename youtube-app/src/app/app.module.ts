b73f733e88954101ea4e47d24e5cfdbd8815247f
MaurickThom <thomtwd@gmail.com>
2020-03-04 02:45:12 -0500

    upgrade packages

--- youtube-app/src/app/app.module.ts
+++ youtube-app/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { HeadComponent } from './components/head/head.component';
+import { HomeComponent } from './components/home/home.component';
+import { YoutubeService } from './services/youtube.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    HeadComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [YoutubeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
