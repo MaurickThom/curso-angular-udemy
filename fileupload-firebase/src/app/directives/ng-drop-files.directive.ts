0929d8d75d287587a38032942adaf914bd41283e
MaurickThom <thomtwd@gmail.com>
2020-03-05 17:49:51 -0500

    validations

--- fileupload-firebase/src/app/directives/ng-drop-files.directive.ts
+++ fileupload-firebase/src/app/directives/ng-drop-files.directive.ts
@@ -6,6 +6,7 @@ import {
         Input,
         Output
       } from '@angular/core';
+import { FileItems } from '../models/file-items';
 
 // ElementRef : mantiene una relacion directa con el elemento que contiene la directiva
 // HostListener : crear callbacks
@@ -15,6 +16,7 @@ import {
 })
 export class NgDropFilesDirective {
 
+  @Input('files') files:FileItems[] = []
   @Output() mouseUp : EventEmitter<boolean> = new EventEmitter()
 
   // Cuando se esta arrastrando a el y dispara un evento
@@ -34,13 +36,35 @@ export class NgDropFilesDirective {
   // soltar
   @HostListener('drop',['$event'])
   public onDrop(event:any){
+    const dataTransfer = this._getDataTransfer(event)
+
+    if(!dataTransfer) return
+
+
 
     this.mouseUp.emit(false)
   }
 
-  //
+  // Validaciones
+
+  // tenemos que evitar que el navegador habra por defecto la imagen despues de arrastrar a soltar
+  private _preventOpenImage = (event:any) => {
+    event.preventDefault()
+    event.stopPropagation()
+  }
+
+  // validar que la imagen ya este en la caja antes de enviar a firebase
+  private _fileAlreadyPublished(nameFile:string):boolean{
+    return this.files.filter(file=>file.nameFile===nameFile).length ? true : false
+  }
+
   private _getDataTransfer = (event:any)=>(
     event.dataTransfer ? event.dataTransfer : event.originalEvent.dataTransfer
   )
 
+  // validacion para ver si es imagen
+  private _isImage = (typeOfFile:string):boolean =>(
+    ( typeOfFile === ''  || typeOfFile === undefined ) ?
+      false : typeOfFile.startsWith('image')
+  )
 }
