8613164ddd7c040654c834df9c5338d8ad034ef8
MaurickThom <thomtwd@gmail.com>
2020-03-06 01:15:44 -0500

    update

--- dynamic-graphics/src/app/components/doughnut/doughnut.component.ts
+++ dynamic-graphics/src/app/components/doughnut/doughnut.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Label, MultiDataSet } from 'ng2-charts';
+import { ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-doughnut',
@@ -6,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./doughnut.component.scss']
 })
 export class DoughnutComponent implements OnInit {
-
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100]
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
   constructor() { }
 
   ngOnInit(): void {
