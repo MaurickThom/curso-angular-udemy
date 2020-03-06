2ca5f5acc8de8d94ea9ed63f1c74f501b7007473
MaurickThom <thomtwd@gmail.com>
2020-03-05 19:57:38 -0500

    service loadImage

--- fileupload-firebase/src/app/services/upload-photos.service.ts
+++ fileupload-firebase/src/app/services/upload-photos.service.ts
@@ -20,8 +20,6 @@ export class UploadPhotosService {
 
   }
   loadImagesFirebase(images:FileItems[]){
-    console.log(images);
-
     const storegeRef = firebase.storage().ref()
 
     for(let image of images){
@@ -29,6 +27,28 @@ export class UploadPhotosService {
       if( image.progress > 0 )
         continue
 
+      const uploadTask:firebase.storage.UploadTask = storegeRef.child(
+        `${this.DIR_IMAGES}/${image.nameFile}`
+      ).put(image.file)
+
+      uploadTask.on(
+        firebase.storage.TaskEvent.STATE_CHANGED,
+        (snapshot:firebase.storage.UploadTaskSnapshot)=>{ // evento asincrono hasta que cargue los bits para subir la imagen
+          image.progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        },
+        error=>console.error('Error al subir a firebase ',error),
+        ()=>{ // está funcion se ejecutara cuando la carga de bits de cada imagen se ejecuto correctamente
+          uploadTask.snapshot.ref.getDownloadURL().then(downloadURL=>{
+            console.log('file available at ',downloadURL);
+            image.url = downloadURL
+            image.isUpload = false
+            this.saveImages({
+              name:image.nameFile,
+              url:image.url
+            })
+          })
+        }
+      )
     }
 
   }
