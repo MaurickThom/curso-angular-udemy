263210b72f7011d582ec62c8a5f4f24978b032e3
MaurickThom <thomtwd@gmail.com>
2020-03-06 12:05:53 -0500

    virtual scroll

--- virtual-scroll-and-drag-drop/src/app/components/virtual-scroll/virtual-scroll.component.ts
+++ virtual-scroll-and-drag-drop/src/app/components/virtual-scroll/virtual-scroll.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-virtual-scroll',
@@ -7,11 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VirtualScrollComponent implements OnInit {
 
+  @ViewChild(CdkVirtualScrollViewport) viewport: CdkVirtualScrollViewport
+  inicio:boolean = true
   constructor() { }
   persons = Array(500).fill(0)
   // persons =  Array.from({length: 100000})
   ngOnInit(): void {
     // console.log(this.persons);
   }
-
+  irFinal(){
+    this.viewport.scrollToIndex(this.persons.length)
+    this.inicio = false
+  }
+  irInicio(){
+    this.viewport.scrollToIndex(0)
+    this.inicio = true
+  }
+  irAlMedio(){
+    this.viewport.scrollToIndex(this.persons.length/2)
+  }
 }
